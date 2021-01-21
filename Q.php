e47e47b82c2770c50144ed1c6840dc78925fc9ed
mustaphazdali <mustaphazdali@gmail.com>
2021-01-21 12:29:07 +0100

    initial git Repo &  rename the files

---
+++ Q.php
@@ -0,0 +1,5 @@
+ <br />
+<b>Fatal error</b>:  Uncaught Error: Call to a member function bind_param() on bool in C:\xampp\htdocs\greefa\Q.php:8
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\greefa\Q.php</b> on line <b>8</b><br />
